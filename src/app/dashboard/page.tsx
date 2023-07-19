77a89bd8f373f25f4b2fcfda920396a67ad90f48
Asfandyar <asfandm17@gmail.com>
2023-07-19 11:31:59 +0500

    chartBox container

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import ChartBox from "@components/chartBox/ChartBox";
 import Deals from "@components/topDeals/Deals";
 import { Metadata } from "next";
 import React from "react";
@@ -13,10 +14,18 @@ const Dashboard = () => {
       <div className="box box1">
         <Deals />
       </div>
-      <div className="box box2">Box2</div>
-      <div className="box box3">Box3</div>
-      <div className="box box4">Box4</div>
-      <div className="box box5">Box5</div>
+      <div className="box box2">
+        <ChartBox />
+      </div>
+      <div className="box box3">
+        <ChartBox />
+      </div>
+      <div className="box box4">
+        <ChartBox />
+      </div>
+      <div className="box box5">
+        <ChartBox />
+      </div>
       <div className="box box6">Box6</div>
       <div className="box box7">Box7</div>
       <div className="box box8">Box8</div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
 import "./home.scss";
 
 export default function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam quod
+        pariatur omnis eos libero veniam quae possimus laborum, repellendus in
+        asperiores deserunt molestias iure minima accusamus impedit rerum sint
+        esse, atque alias necessitatibus. Illum ad officia, unde optio quia
+        neque repellendus, similique aliquid sequi nostrum odit reprehenderit
+        corporis provident consequatur? Ducimus placeat veniam corrupti et
+        doloremque modi dolores debitis perspiciatis aut architecto iure saepe
+        provident eaque excepturi, at praesentium perferendis optio suscipit
+        nostrum quidem temporibus neque quam accusantium. In sit inventore illo
+        accusamus velit voluptatum perspiciatis ipsa perferendis, fuga quibusdam
+        officiis aliquam aspernatur delectus neque est reiciendis tenetur
+        maiores. Fugit?
+      </p>
+    </div>
+  );
 }
